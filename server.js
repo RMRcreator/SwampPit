32481d6534f7f0e7988adaf735f731fe8317df0c
LTK107 <alexisjovon@gmail.com>
2025-09-27 15:03:04 -0400

    Added the base of profiles page

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
+const path = require('path');
+
+//using this to make the profile pages dynamic
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
 // routing path
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+
+//this will take us to the profiles page
+app.get('/profiles', (req, res) => {
+    res.render('profiles', {topic: 'People profiles'});
+    //res.send("It should have apunch of profiles on here");
+});
+
 
 // Start the server
 app.listen(3000, () => {
