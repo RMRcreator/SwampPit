c5ac57009f95a52c880cd90344a045c6619e054c
Kaydence Rao <dencer797@gmail.com>
2025-09-27 22:01:28 -0400

    Fixed the server.js

--- server.js
+++ server.js
@@ -71,49 +71,7 @@ app.post('/set-profile', async (req, res) => {
     }
 })
 
-
-// routing path to home page
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-//this will take us to the profiles page
-app.get('/profiles', async (req, res) => {
-    try {
-        const profiles = await Profile.find();   // fetch profiles from MongoDB
-        res.render('profileList', { topic: 'People profiles', profiles });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error fetching profiles');
-    }
-});
-
-
-
-app.get('/setup', (req, res) =>{
-    res.render('ProfileCreation');
-})
-
-app.post('/set-profile', async (req, res) => {
-    try {
-        const newProfile = new Profile({
-            name: req.body.name,
-            age: req.body.age,
-            year: req.body.year,
-            major: req.body.major,
-            classes: req.body.classes,
-            interests: req.body.interests
-        })
-
-        await newProfile.save()
-        res.redirect('/profiles') 
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('Error saving profile')
-    }
-})
-
+// sign up
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, "html_files", "signup.html"))
 })
